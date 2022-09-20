4edeee6b36e4b0ee9c68c284954b588899c4a403
Joseph <wisdombegins78@gmail.com>
2022-09-20 10:13:08 -0500

    Added mobile menu, javascript and some more css styling

--- assets/script..js
+++ assets/script..js
@@ -1,42 +1,10 @@
-const toggle = document.querySelector(".toggle");
-const menu = document.querySelector(".menu");
- 
-/* Toggle mobile menu */
-function toggleMenu() {
-    if (menu.classList.contains("active")) {
-        menu.classList.remove("active");
-         
-        // adds the menu (hamburger) icon
-        toggle.querySelector("a").innerHTML = "<i class=’fas fa-bars’></i>";
-    } else {
-        menu.classList.add("active");
-         
-        // adds the close (x) icon
-        toggle.querySelector("a").innerHTML = "<i class=’fas fa-times’></i>";
-    }
-}
- 
-/* Event Listener */
-toggle.addEventListener("click", toggleMenu, false);
 
-const items = document.querySelectorAll(".item");
- 
-/* Activate Submenu */
-function toggleItem() {
-  if (this.classList.contains("submenu-active")) {
-    this.classList.remove("submenu-active");
-  } else if (menu.querySelector(".submenu-active")) {
-    menu.querySelector(".submenu-active").classList.remove("submenu-active");
-    this.classList.add("submenu-active");
-  } else {
-    this.classList.add("submenu-active");
-  }
+const mobileMenu = document.querySelector(".mobile-menu"); 
+const mainNav = document.querySelector(".main-nav"); 
+console.log(mobileMenu, mainNav)
+const openMenu = () => {
+  mainNav.classList.toggle('is-active');
+
 }
- 
-/* Event Listeners */
-for (let item of items) {
-    if (item.querySelector(".submenu")) {
-      item.addEventListener("click", toggleItem, false);
-      item.addEventListener("keypress", toggleItem, false);
-    }   
-}
+
+mobileMenu.addEventListener('click', openMenu);
