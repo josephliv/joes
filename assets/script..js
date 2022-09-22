545fb866f0a4ed105f7ed970b016271993c583a2
Joseph <wisdombegins78@gmail.com>
2022-09-22 17:32:42 -0500

    renamed the tags to sections and added body tag for formatting

--- assets/script..js
+++ assets/script..js
@@ -14,3 +14,29 @@ mobileMenu.addEventListener('click', openMenu);
 if (window.innerWidth > '765' && mainNav.classList.contains('is-active') ) {
   mainNav.classList.remove('is-active');
 }
+
+// Back to the top button 
+ //Get the button
+ const mybutton = document.getElementById("myBtn");
+
+ // When the user scrolls down 20px from the top of the document, show the button
+ window.onscroll = function () {
+   scrollFunction();
+ };
+
+ function scrollFunction() {
+   if (
+     document.body.scrollTop > 80 ||
+     document.documentElement.scrollTop > 80
+   ) {
+     mybutton.style.display = "block";
+   } else {
+     mybutton.style.display = "none";
+   }
+ }
+
+ // When the user clicks on the button, scroll to the top of the document
+ function topFunction() {
+   document.body.scrollTop = 0;
+   document.documentElement.scrollTop = 0;
+ }
