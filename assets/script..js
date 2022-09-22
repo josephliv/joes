2055517924287ae6d1f1251d8857d281bb731ff0
Joseph <wisdombegins78@gmail.com>
2022-09-22 16:13:09 -0500

    Added a few of the portfolio images mostly for testing, added new logo image.

--- assets/script..js
+++ assets/script..js
@@ -4,7 +4,13 @@ const mainNav = document.querySelector(".main-nav");
 console.log(mobileMenu, mainNav)
 const openMenu = () => {
   mainNav.classList.toggle('is-active');
-
 }
 
+
 mobileMenu.addEventListener('click', openMenu);
+
+// My attempt to make sure the class is removed if the window is more than 765px width, 
+// but it's missing something, I'll come back to this later
+if (window.innerWidth > '765' && mainNav.classList.contains('is-active') ) {
+  mainNav.classList.remove('is-active');
+}
